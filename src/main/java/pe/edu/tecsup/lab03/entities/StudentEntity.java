6e10910e0d7769dce310fd0469febf0d99c78e8a
roberto lopez <roberto.lopez@tecsup.edu.pe>
2025-09-02 14:50:47 -0500

    subiendo cambios de entity

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,4 +1,11 @@
 package pe.edu.tecsup.lab03.entities;
 
 public class StudentEntity {
+
+    public String sumarConMensaje(int a, int b) {
+        int suma = a + b;
+        return "La suma es buena: " + suma;
+    }
+
 }
+
